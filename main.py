70e3d1902514f5e21daa9c5343eb9371ef9f0ffc
AntonSkyrda <mr.skyrda@gmail.com>
2024-04-16 15:42:00 +0300

    Solution

---
+++ main.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+import crud
+import schemas
+from db.engine import SessionLocal
+
+app = FastAPI()
+
+
+def get_db() -> Session:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/")
+def api_root() -> dict:
+    return {"message": "Hello World"}
+
+
+@app.get("/author/{id}", response_model=schemas.Author)
+def get_author(author_id: int, db: Session = Depends(get_db)):
+    db_author = crud.get_author(db=db, author_id=author_id)
+
+    if db_author is None:
+        raise HTTPException(status_code=404, detail="Author not found")
+
+    return db_author
+
+
+@app.get("/authors/", response_model=list[schemas.Author])
+def get_authors(
+        db: Session = Depends(get_db),
+        skip: int = 0,
+        limit: int = 100
+):
+    return crud.get_all_authors(db=db, skip=skip, limit=limit)
+
+
+@app.post("/authors/", response_model=schemas.AuthorCreate)
+def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+    return crud.create_author(db=db, author=author)
+
+
+@app.get("/book/{id}", response_model=schemas.Book)
+def get_book(book_id: int, db: Session = Depends(get_db)):
+    db_book = crud.get_book(db=db, book_id=book_id)
+
+    if db_book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    return db_book
+
+
+@app.get("/books/", response_model=list[schemas.Book])
+def get_books(
+        db: Session = Depends(get_db),
+        author_id: int | None = None,
+        skip: int = 0,
+        limit: int = 100
+):
+    return crud.get_all_books(
+        db=db,
+        author_id=author_id,
+        skip=skip,
+        limit=limit
+    )
+
+
+@app.post("/books/", response_model=schemas.BookCreate)
+def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
+    return crud.create_book(db=db, book=book)
